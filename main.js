92bdedfe59d1484cd67e3f6e1ce07b73fef35405
noleemits <stevenoleemits@gmail.com>
2023-01-09 21:38:33 -0600

    Initial setup

---
+++ main.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelector(".tabs");
+
+tabs.addEventListener("click", (e) => handleClick(e));
+
+function handleClick(e) {
+  const target = e.target;
+  const tabNum = target.dataset.tab;
+  const activeTab = document.querySelector(".tabs .active");
+  const activeContent = document.querySelector(".tab-content .visible");
+  const currentContent = document.querySelector(`.tab-content__section[data-tab='${tabNum}']`);
+
+  if (!tabNum) {
+    return;
+  }
+
+  activeTab.classList.remove("active");
+  target.classList.add("active");
+  activeContent.classList.remove("visible");
+  currentContent.classList.add("visible");
+}
